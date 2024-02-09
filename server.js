b4119595080f55c0833b292b9e8d8bd2756394a3
Valete_ <valent1dj91@gmail.com>
2024-02-09 14:53:04 +0100

    added channel specific time to delete

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const channelList = require('./channels.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -15,10 +16,12 @@ const clearMessage = async () => {
   const channels = await guild.channels.fetch();
   for (const channel of channels.values()) {
     if (channel.type != 0) continue
+    let channelInfo = channelList.find(c => c.id == channel.id)
+    if (!channelInfo) continue
     const messages = await channel.messages.fetch();
     for (const message of messages.values()) {
       if (message.author.system) continue
-      if (message.createdTimestamp < Date.now() - 28800000) /* 28800000 */ {
+      if (message.createdTimestamp < Date.now() - channelInfo.timeout) /* 28800000 */ {
         message.delete()
         deleted++
       }
